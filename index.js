60f4d3b6c2100ec298fd5b6ebb7df3b19a5bf480
seun-o <12373359+Seun-O@users.noreply.github.com>
2019-08-14 22:43:46 -0400

    initil push

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const express = require("express");
+// const cors = require('cors')
+// const helmet = require('helmet')
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.status(200).json("Server is up");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is up on http://localhost:${PORT}`);
+});
