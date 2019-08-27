ba6141fbdb23fbd98c09f43ffdd9549f8e6b2334
LD <LDinho@users.noreply.github.com>
2019-08-26 20:21:19 -0400

    Add GET school by id endpoint

--- routes/schools/index.js
+++ routes/schools/index.js
@@ -19,4 +19,26 @@ router.get('/', async (req, res) => {
   }
 })
 
+// GET School by Id
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const school = await db.getSchoolById(id);
+
+    if (!school) {
+      return res.status(404).json({message: 'school not found'});
+    }
+
+    res.status(200).json(school);
+  }
+  catch (err) {
+    res.status(500)
+      .json({
+        err,
+        message: 'Unable to process request'
+      })
+  }
+});
+
 module.exports = router;
