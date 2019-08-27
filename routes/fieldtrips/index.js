f31853fadf46f8d89b8ca0636668341e5876615b
stevekutz <stkutz@gmail.com>
2019-08-26 20:38:21 -0500

    added post

--- routes/fieldtrips/index.js
+++ routes/fieldtrips/index.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
         const fieldtrips = await db.getFieldTrips();
         res.status(200).json(fieldtrips)
     }
-    catch{
+    catch(error){
        res.status(500).json({
            message: `error getting fieldtrips`,
            error: error 
@@ -26,7 +26,7 @@ router.get('/:id', async (req, res) => {
             res.status(404).json({message: `the fieldtrip with id ${id} does not exist`})
         }
     }
-    catch{
+    catch(error){
         res.status(500).json({
             message: `fieldtrips Server Error `,
             error: error 
@@ -34,5 +34,56 @@ router.get('/:id', async (req, res) => {
      }
 
 })
+
+router.post('/', async(req, res) => {
+    const {name} = req.body;
+    
+    try{
+        if(name === '') {
+            res.status(400).json({message: `Please provide name`});
+        } else {
+            const fieldTrip = await db.addFieldTrip(req.body);
+            res.status(201).json(fieldTrip);
+        }
+    }
+    catch(error){
+        res.status(500).json({
+            message: `fieldtrips Server Error `,
+            error: error 
+         })
+     }
+
+
+})
+
+
+router.put('/:id', async(req, res) => {
+
+    const updatedTripInfo = req.body;
+    const {id} = req.params;
+
+    try {
+        const fieldTripUpdated = await db.updateFieldTrip(id, updatedTripInfo);
+
+        if(fieldTripUpdated) {
+            res.status(200).json(updatedTripInfo);
+        } else {
+            res.status(404).json({
+                message: `Field Trip with id ${id} does not exist`
+            })
+        }
+
+    }
+    catch(error){
+        res.status(500).json({
+            message: `fieldtrips Server Error `,
+            error: error 
+         })
+     }
+
+});
+
+
+
 
 module.exports = router;
