9d7673c659c6de7507c5d6dc99b435597ba4c196
stevekutz <stkutz@gmail.com>
2019-08-26 23:26:29 -0500

    added POST, PUT, DELETE student endpoints

--- routes/students/index.js
+++ routes/students/index.js
@@ -34,9 +34,70 @@ router.get('/:id', async (req, res) => {
      }
 })
 
+router.post('/', async(req, res) => {
+    const {first_name, last_name} = req.body;
+    
+    try{
+     if(first_name === '' || last_name === '') {
+           res.status(400).json({message: `Please provide first AND last name`});
+       } else {
+            const student = await db.addStudent(req.body);
+            res.status(201).json(student);
+      }
+    } catch(error){
+        res.status(500).json({
+            message: `students Server Error `,
+            error: error 
+         })
+     }
+})
 
+router.delete('/:id', async(req, res) => {
+    const {id} = req.params;
 
+    try {
+        const allStudents = await db.deleteStudent(id);
+        console.log('>>>>>>>>', allStudents);
 
+        if(allStudents) {
+            res.status(200).json({ 
+                message: `Student with id ${id} has been deleted`
+            })
+        } else {
+            res.status(404).json({message: `the student with id ${id} does not exist`})
+        }
+    
+    } catch(error){
+        res.status(500).json({
+            message: `student Server Error `,
+            error: error 
+         })
+     }
+})
 
+router.put('/:id', async(req, res) => {
+
+    const updatedStudentInfo = req.body;
+    const {id} = req.params;
+
+    try {
+        const studentUpdated = await db.updateStudent(id, updatedStudentInfo);
+
+        if(studentUpdated) {
+            res.status(200).json(updatedStudentInfo);
+        } else {
+            res.status(404).json({
+                message: `Student with id ${id} does not exist`
+            })
+        }
+
+    } catch(error){
+        res.status(500).json({
+            message: `students Server Error `,
+            error: error 
+         })
+     }
+
+});
 
 module.exports = router;
