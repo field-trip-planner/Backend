b8c06484b06bd7d39df2fa5276a3b4bf5b9039cf
LD <LDinho@users.noreply.github.com>
2019-08-26 20:19:29 -0400

    add better handling of errors to GET schools endpoint

--- routes/schools/index.js
+++ routes/schools/index.js
@@ -2,18 +2,21 @@ const express = require('express');
 const db = require('../../models/schoolModel/index');
 const router = express.Router();
 
+// GET Schools
 router.get('/', async (req, res) => {
-  try{
+  try {
     const schools = await db.getSchools();
-    res.status(200).json(schools)
+
+    if (schools.length) {
+      res.status(200).json(schools)
+
+    } else {
+      res.status(404).json({message: `No schools found`})
+    }
   }
-  catch{
-    res.status(500).json({
-      message: `error getting schools`,
-      error: error
-    })
+  catch (err) {
+    res.status(500).json({error: `Unable to retrieve schools`})
   }
 })
-
 
 module.exports = router;
