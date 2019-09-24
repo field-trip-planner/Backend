1aca60e501c8d24ffd7fdde9e13441c9155865a6
Eiosx <eiosxenzo@gmail.com>
2019-09-23 22:08:02 -0400

    Added getChaperonesBySchoolId method and route so the teacher can have a list of chaperones to choose from for their field trip.

--- models/userModel/index.js
+++ models/userModel/index.js
@@ -10,13 +10,14 @@ const getUserById = id => {
     .first();
 };
 
-//
-const getUserBySchoolId = (schoolId) => {
+//Parent-Student Relationship
+const getUserParentBySchoolId = (schoolId) => {
   return db("users")
   .where({school_id: schoolId, role: 'parent'})
   .orderBy("last_name");
 };
 
+//Get all chaperones by school id so that the Teacher can add them to field trip
 const getUserChaperoneBySchoolId = schoolId => {
   return db('users')
   .where({school_id: schoolId, role: 'chaperone'})
@@ -51,7 +52,7 @@ const deleteUser = id => {
 module.exports = {
   getUsers,
   getUserById,
-  getUserBySchoolId,
+  getUserParentBySchoolId,
   getUserChaperoneBySchoolId,
   addUser,
   updateUser,
--- routes/users/index.js
+++ routes/users/index.js
@@ -39,7 +39,7 @@ router.get("/school/:id", async (req, res) => {
 
   try {
 
-    const users = await db.getUserBySchoolId(id)
+    const users = await db.getUserParentBySchoolId(id)
     res.status(200).json({ users: users });
     
   } catch (error) {
