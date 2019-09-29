9f81da4c9d90f2dc22899fe0fca406e527f0fcf4
LD <LDinho@users.noreply.github.com>
2019-09-29 19:54:59 -0400

    remove the async key word

--- models/chaperones_field_tripsModel/index.js
+++ models/chaperones_field_tripsModel/index.js
@@ -1,9 +1,9 @@
 const db = require("../../db");
 
 //Get Chaperone's Field Trips
-async function getChaperoneFieldTripsById(id){
+function getChaperoneFieldTripsById(id){
   return db("chaperones_field_trips")
-  .where({ user_id: id })
+  .where({ user_id: id });
 }
 
 module.exports = {
